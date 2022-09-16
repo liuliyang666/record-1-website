56c327221bc82df8a9e59ca5054d966206d2aa02
lilyyang <1592218619@qq.com>
2022-09-16 15:26:14 +0800

    deploy

---
+++ precache-manifest.0c12b15528a39bba11c6871836fb707c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6254f1ad9313be7de30",
+    "url": "/record-1-website/css/app.a9adfcc8.css"
+  },
+  {
+    "revision": "bf02bb8c07d54b91b366f0b3cf4c17b8",
+    "url": "/record-1-website/index.html"
+  },
+  {
+    "revision": "c6254f1ad9313be7de30",
+    "url": "/record-1-website/js/app.cbb30b4f.js"
+  },
+  {
+    "revision": "09459423cacf10890b26",
+    "url": "/record-1-website/js/chunk-vendors.36215478.js"
+  },
+  {
+    "revision": "d64761912105d2fb52985f5ca3784a0d",
+    "url": "/record-1-website/manifest.json"
+  },
+  {
+    "revision": "adc06bc55b65ee28078fce2f428d2645",
+    "url": "/record-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/record-1-website/robots.txt"
+  }
+]);
